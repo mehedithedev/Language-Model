4efa485033fc327a06f581f6b1bef0abb1714b04
mehedithedev <mehedi.developer@icloud.com>
2024-10-01 15:27:46 -0600

    dumped language model

--- training_ml_model.py
+++ training_ml_model.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import joblib
 
 music_data = pd.read_csv('music.csv')
 
@@ -9,15 +10,21 @@ X = music_data.drop(columns=['genre']) # dropping the target values
 
 y = music_data['genre'] # target or output values
 
-model = DecisionTreeClassifier()
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
-model.fit(X_train, y_train)
+# model = DecisionTreeClassifier()  # been using before making our own model
 
-predictions = model.predict(X_test)
+model = joblib.load('music-recommender.joblib')
+
+
+# We already trained our model 
+# model.fit(X_train, y_train)
+
+
+# Creating our own model
+# joblib.dump(model, 'music-recommender.joblib')
+
+predictions = model.predict([[21,1], [22,0], [120, 0], [29,0]])
 print(predictions)
 
-score = accuracy_score(y_test, predictions)
-
-print(score)
+# score = accuracy_score(y_test, predictions)
